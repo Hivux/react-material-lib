dde669dbd9e7fc3dbf7200e68ae2d03ac83d77d4
Jorge Franco <mail@lipsoft.pt>
2020-06-13 18:23:19 +0000

    Initial Setup with a special Component "Gordo"

---
+++ src/gordo/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+export const Gordo = ({ text }) => {
+  return <div className={styles.test}>Gordo Component: {text}</div>
+}
---
+++ src/gordo/index.test.js
@@ -0,0 +1,7 @@
+import { Gordo } from '.'
+
+describe('Gordo', () => {
+  it('is truthy', () => {
+    expect(Gordo).toBeTruthy()
+  })
+})
