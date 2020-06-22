462c400977e0e1c7635cb58c8de626932be304c9
Jorge Franco <mail@lipsoft.pt>
2020-06-22 12:40:39 +0000

    fix

--- example/src/App.test.js
+++ example/src/App.test.js
@@ -1,4 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-test('CI Test delete in the future', () => {
-  expect(true).toBeTruthy()
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+  ReactDOM.unmountComponentAtNode(div)
 })
